6ad5379f210245576b0813fd5b94e5aec687b72e
qansari <quinn.ansari@solers.com>
2019-11-12 14:11:02 -0500

    5
    Added basic login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import * as loginActionCreator from "../../actions/login";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-
+import { Button, Form, Container, Row, Col, Card } from "react-bootstrap";
 class Login extends React.Component {
   constructor(props) {
     super(props);
@@ -32,37 +30,69 @@ class Login extends React.Component {
   }
   render() {
     return (
-      <Form>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            name="email"
-            type="email"
-            placeholder="Enter email"
-            value={this.state.email}
-            onChange={this.handleInputChange}
-          />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            name="password"
-            type="password"
-            placeholder="Password"
-            value={this.state.password}
-            onChange={this.handleInputChange}
-          />
-        </Form.Group>
-        <Form.Group controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" onClick={() => this.handleSubmit()}>
-          Submit
-        </Button>
-      </Form>
+      <Container>
+        <Row>
+          <Col xs={1} md={4}></Col>
+          <Col>
+            <img
+              width={75}
+              height={75}
+              className="mr-3"
+              src="/logo192.png"
+              alt=""
+            />
+            <h1>PointAverage</h1>
+          </Col>
+          <Col xs={1} md={4}></Col>
+        </Row>
+
+        <Container>
+          <Row className="show-grid">
+            <Col xs={1} md={4}></Col>
+            <Col xs={4} md={4}>
+              <div class="shadow p-3 mb-5 bg-white rounded">
+                <Card style={{ width: "18rem", border: "none" }}>
+                  <Card.Body>
+                    <Card.Title>Sign in</Card.Title>
+                    <Form>
+                      <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control
+                          name="email"
+                          type="email"
+                          placeholder="Enter email"
+                          value={this.state.email}
+                          onChange={this.handleInputChange}
+                        />
+                        <Form.Text className="text-muted">
+                          We'll never share your email with anyone else.
+                        </Form.Text>
+                      </Form.Group>
+                      <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                          name="password"
+                          type="password"
+                          placeholder="Password"
+                          value={this.state.password}
+                          onChange={this.handleInputChange}
+                        />
+                      </Form.Group>
+                      <Button
+                        variant="primary"
+                        onClick={() => this.handleSubmit()}
+                      >
+                        Submit
+                      </Button>
+                    </Form>
+                  </Card.Body>
+                </Card>
+              </div>
+            </Col>
+            <Col xs={1} md={4}></Col>
+          </Row>
+        </Container>
+      </Container>
     );
   }
 }
